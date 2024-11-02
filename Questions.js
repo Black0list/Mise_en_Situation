735fd9721969d3ea0dae7ccdba2dfa303b5ffb3d
ABDELKEBIR HADOUI <abdelkebir123123@gmail.com>
2024-11-02 12:45:04 +0100

    fixed Colappsed Problem

--- Questions.js
+++ Questions.js
@@ -1,21 +1,8 @@
 
 
-let Questions = [
-    {
-        "question" : "q1",
-        "reponse" : "r1",
-    },
-    {
-        "question" : "q2",
-        "reponse" : "r2",
-    },
-    {
-        "question" : "q3",
-        "reponse" : "r3",
-    }
-]
+let Questions = []
 
-var NbrQ = 3;
+var NbrQ = Questions.length;
 let Index = 0;
 
 
@@ -44,17 +31,17 @@ function addQuestion(){
     accordion_item.appendChild(accordion_h2);
 
     const accordion_btn = document.createElement("button");
-    accordion_btn.classList.add("accordion-button");
+    accordion_btn.classList.add("accordion-button", "collapsed");
     accordion_btn.type = "button";
     accordion_btn.setAttribute("data-bs-toggle", "collapse");
     accordion_btn.setAttribute("data-bs-target", `#collapse${i}`);
-    accordion_btn.setAttribute("aria-expanded", "false");
+    accordion_btn.setAttribute("aria-expanded", "true");
     accordion_btn.setAttribute("aria-controls", `collapse${i}`);
     accordion_btn.innerText = e.question;
     accordion_h2.appendChild(accordion_btn);
 
     const accordion_collapse = document.createElement("div");
-    accordion_collapse.classList.add("accordion-collapse", "collapse", "show");
+    accordion_collapse.classList.add("accordion-collapse", "collapse");
     accordion_collapse.setAttribute("id", `collapse${i}`);
     accordion_collapse.setAttribute("data-bs-parent", "#accordionExample");
     accordion_item.appendChild(accordion_collapse);
